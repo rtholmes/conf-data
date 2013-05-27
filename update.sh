6539256df9ca2c65aa8b46150b64bc7e39956461
reid holmes <rtholmes@gmail.com>
2013-05-27 16:33:58 -0400

    update update script; trying to prevent excess versionupdates

--- update.sh
+++ update.sh
@@ -1,11 +1,19 @@
 #/bin/sh
 
 echo "Pulling from github"
-git pull
+git pull | grep 'Already up-to-date.'
+
+# if git pull returns 'Already up-to-date.' we should skip the rest
 
 
-DATES=`date +%s`
+if [ $$ != '' ];
+then
+	echo "Metadata up-to-date; no version update required."
+else
+	DATES=`date +%s`
 
-echo "New date string: $DATES"
-# {"LastUpdatePOSIX" : "1363472766"}
-echo "{\"LastUpdatePOSIX\" : \"$DATES\"}" > IndexVersion.json;
+	echo "New date string: $DATES"
+	# {"LastUpdatePOSIX" : "1363472766"}
+	echo "{\"LastUpdatePOSIX\" : \"$DATES\"}" > IndexVersion.json;	
+fi
+	
