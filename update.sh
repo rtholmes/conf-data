76f26c1ca198350e5d8e5f4016d960546b5de676
Reid Holmes (MBA) <rtholmes@gmail.com>
2013-09-15 05:45:57 -0400

    change update.sh echo

--- update.sh
+++ update.sh
@@ -1,19 +1,19 @@
 #/bin/sh
 
-echo "Pulling from github"
-git pull | grep 'Already up-to-date.'
+#echo "Pulling from github"
+#git pull | grep 'Already up-to-date.'
 
 # if git pull returns 'Already up-to-date.' we should skip the rest
 
 
-if [ $$ != '' ];
-then
-	echo "Metadata up-to-date; no version update required."
-else
+#if [ $$ != '' ];
+#then
+#	echo "Metadata up-to-date; no version update required."
+#else
 	DATES=`date +%s`
 
 	echo "New date string: $DATES"
 	# {"LastUpdatePOSIX" : "1363472766"}
 	echo "{\"LastUpdatePOSIX\" : \"$DATES\"}" > IndexVersion.json;	
-fi
+#fi
 	
